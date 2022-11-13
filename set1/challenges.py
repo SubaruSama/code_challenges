3a9e4b7e8c0cb9acc26db8bd887eb089b6dcd253
SubaruSama <andresfauqe@gmail.com>
2022-11-12 23:51:21 -0300

    Morrendo de sono
    Commitando para não perder caso falte luz

--- set1/challenges.py
+++ set1/challenges.py
@@ -5,6 +5,7 @@ import binascii
 
 def hex_to_base(string: str) -> str:
     """
+    Challenge 1
     Convert hex to base64
     The string: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
     Should produce: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
@@ -39,3 +40,21 @@ def hex_to_base(string: str) -> str:
     print(f'From hex to base64 and type: {b64_string} of type {(type(b64_string))}')
 
     return b64_string.decode('utf-8')
+
+def fixed_xor(first_xor_string: str, second_xor_string: str):
+    """
+    Challenge 2
+
+    Write a function that takes two equal-length buffers and produces their XOR combination.
+
+    If your function works properly, then when you feed it the string: 1c0111001f010100061a024b53535009181c
+
+    ... after hex decoding, and when XOR'd against: 686974207468652062756c6c277320657965
+
+    ... should produce: 746865206b696420646f6e277420706c6179
+    """
+
+    # Checar se o tamanho é o mesmo. XOR não consegue funcionar se tiver tamanhos diferentes.
+    assert len(first_xor_string) == len(second_xor_string)
+
+    raise NotImplementedError
--- set1/test_challenges.py
+++ set1/test_challenges.py
@@ -1,5 +1,5 @@
 import unittest
-from challenges import hex_to_base
+from challenges import hex_to_base, fixed_xor
 
 class Set1TestCase(unittest.TestCase):
 
@@ -7,6 +7,11 @@ class Set1TestCase(unittest.TestCase):
         self.hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
         self.b64_string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
         self.assertEqual(hex_to_base(self.hex_string), self.b64_string)
+
+    def test_fixed_xor(self):
+        self.first_xor_string = '1c0111001f010100061a024b53535009181c'
+        self.second_xor_string = '686974207468652062756c6c277320657965'
+        self.result = '746865206b696420646f6e277420706c6179'
 
 if __name__ == '__main__':
     unittest.main()
