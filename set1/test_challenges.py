ac66e4bcc7249f23be434ff44d56f696a46e5390
SubaruSama <andresfauqe@gmail.com>
2022-11-14 13:14:47 -0300

    Escrevendo testes para garantir que a função fixed_xor cheque o tamanho das strings

--- set1/test_challenges.py
+++ set1/test_challenges.py
@@ -1,5 +1,7 @@
 import unittest
-from challenges import hex_to_base, fixed_xor
+
+from challenges import fixed_xor, hex_to_base
+
 
 class Set1TestCase(unittest.TestCase):
 
@@ -12,6 +14,14 @@ class Set1TestCase(unittest.TestCase):
         self.first_xor_string = '1c0111001f010100061a024b53535009181c'
         self.second_xor_string = '686974207468652062756c6c277320657965'
         self.result = '746865206b696420646f6e277420706c6179'
+
+        # Check if the function fixed_xor is checking if the inputs provided
+        # are the same size. If not, it should throw and exception.
+        with self.assertRaises(AssertionError) as context_manager:
+            fixed_xor(self.first_xor_string, self.second_xor_string[:-1])
+        self.assertEqual(context_manager.exception.args[0], 'The inputs MUST be the same size')
+
+        # Check if the return of xored strings are equal to result
         self.assertEqual(fixed_xor(self.first_xor_string, self.second_xor_string), self.result)
 
 if __name__ == '__main__':
